7082db71d35336c9fd3371e5519addfda194b94a
João Tonial <tonialjoao97@gmail.com>
2019-02-23 12:25:08 -0300

    testing the kraken websockets

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,42 @@
 import * as React from "react";
 import { useSetTitle } from "../hooks";
 
-const Dashboard = (props: object) => {
-  useSetTitle("Dahsboard");
+var ex = new WebSocket("wss://ws.kraken.com");
 
-  return <h1>DASH</h1>;
+ex.onopen = () => {
+  ex.send(
+    JSON.stringify({
+      event: "subscribe",
+      pair: ["XBT/EUR"],
+      subscription: {
+        name: "trade"
+      }
+    })
+  );
+};
+
+const Dashboard = (props: object) => {
+  const [current, setCurrent] = React.useState("enstablishing connection");
+  ex.onmessage = evt => {
+    const data = JSON.parse(evt.data);
+    console.log(data);
+    if (Array.isArray(data)) {
+      setCurrent(data[0][0]);
+    }
+  };
+
+  useSetTitle("Dahsboard");
+  return (
+    <>
+      <h1 style={{ color: "#eee" }}>{current}</h1>
+      <button
+        type="button"
+        onClick={() => ex.send(JSON.stringify({ event: "ping" }))}
+      >
+        Ping
+      </button>
+    </>
+  );
 };
 
 export default Dashboard;
