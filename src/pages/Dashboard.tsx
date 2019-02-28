159cf0e5e9e9506e89c93beab06d2f97ef241f2f
João Tonial <tonialjoao97@gmail.com>
2019-02-28 19:58:08 -0300

    test #3

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,20 +2,20 @@ import * as React from "react";
 import api from "../utils/api";
 
 const Dashboard = () => {
-  const [currencies, setCurrencies] = React.useState([{ id: "none" }]);
+  // const [currencies, setCurrencies] = React.useState([{ id: "none" }]);
 
-  console.log(currencies);
+  // console.log(currencies);
 
-  React.useEffect(() => {
-    api
-      .currencies()
-      .then(res => setCurrencies(res.data))
-      .catch(err => {
-        throw new Error(err);
-      });
-  }, []);
+  // React.useEffect(() => {
+  //   api
+  //     .currencies()
+  //     .then(res => setCurrencies(res.data))
+  //     .catch(err => {
+  //       throw new Error(err);
+  //     });
+  // }, []);
 
-  return <h3 style={{ color: "#eee" }}>jjjjj</h3>;
+  return <h3 style={{ color: "#eee" }}>testanoooo</h3>;
 };
 
 export default Dashboard;
