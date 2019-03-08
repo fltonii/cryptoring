8270bd0bf14b12f113d6698f6b95f9abe570f48c
João Tonial <tonialjoao97@gmail.com>
2019-03-07 21:05:52 -0300

    going back from debugging

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,20 +2,24 @@ import * as React from "react";
 import api from "../utils/api";
 
 const Dashboard = () => {
-  // const [currencies, setCurrencies] = React.useState([{ id: "none" }]);
-
-  // console.log(currencies);
-
-  // React.useEffect(() => {
-  //   api
-  //     .currencies()
-  //     .then(res => setCurrencies(res.data))
-  //     .catch(err => {
-  //       throw new Error(err);
-  //     });
-  // }, []);
-
-  return <h3 style={{ color: "#eee" }}>bananas</h3>;
+  const [currencies, setCurrencies] = React.useState([{ id: "none" }]);
+  React.useEffect(() => {
+    api
+      .currencies()
+      .then(res => setCurrencies(res.data))
+      .catch(err => {
+        throw new Error(err);
+      });
+  }, []);
+  return (
+    <ul>
+      {currencies.map(currency => (
+        <li>
+          <h3 style={{ color: "#eee" }}>{currency.id}</h3>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Dashboard;
